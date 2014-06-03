e2ae686cad256f1d216247264eddf3c1ff4e7883
Jameson Quave <jquave@gmail.com>
2014-06-03 09:37:30 -0500

    Part 3

---
+++ TestSwift/APIController.swift
@@ -0,0 +1,62 @@
+//
+//  APIController.swift
+//  TestSwift
+//
+//  Created by Jameson Quave on 6/3/14.
+//  Copyright (c) 2014 JQ Software LLC. All rights reserved.
+//
+
+import UIKit
+
+protocol APIControllerProtocol {
+    func didRecieveAPIResults(results: NSDictionary)
+}
+
+class APIController: NSObject {
+    
+    var data: NSMutableData = NSMutableData()
+    var delegate: APIControllerProtocol?
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+        
+        connection.start()
+    }
+    
+    
+    func connection(connection: NSURLConnection!, didFailWithError error: NSError!) {
+        println("Connection failed.\(error.localizedDescription)")
+    }
+    
+    
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        // Recieved a new request, clear out the data object
+        self.data = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        // Append the recieved chunk of data to our data object
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        // Request complete, self.data should now hold the resulting info
+        // Convert the retrieved data in to an object through JSON deserialization
+        var err: NSError
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        // Now send the JSON result to our delegate object
+        delegate?.didRecieveAPIResults(jsonResult)
+    }
+    
+   
+}
--- TestSwift/AppDelegate.swift
+++ TestSwift/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
-        // Override point for customization after application launch.
+        
         return true
     }
 

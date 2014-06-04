c0571696aaee8c5ac6942e294ba5f5d366ba4382
Jameson Quave <jquave@gmail.com>
2014-06-04 11:33:03 -0500

    Part5 Async image cells

--- TestSwift/SearchResultsViewController.swift
+++ TestSwift/SearchResultsViewController.swift
@@ -13,6 +13,7 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     var api: APIController = APIController()
     @IBOutlet var appsTableView : UITableView
     var tableData: NSArray = NSArray()
+    var imageCache = NSMutableArray()
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,17 +33,42 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
         var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
         
         cell.text = rowData["trackName"] as String
-        
-        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
-        var urlString: NSString = rowData["artworkUrl60"] as NSString
-        var imgURL: NSURL = NSURL(string: urlString)
-        
-        // Download an NSData representation of the image at the URL
-        var imgData: NSData = NSData(contentsOfURL: imgURL)
-        cell.image = UIImage(data: imgData)
+        cell.image = UIImage(named: "Icon76")
+
         
         // Get the formatted price string for display in the subtitle
         var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            
+            // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+            var urlString: NSString = rowData["artworkUrl60"] as NSString
+            //var image: UIImage? = self.imageCache[urlString] as? UIImage
+            var image: UIImage? = self.imageCache.valueForKey(urlString) as? UIImage
+            if( !image? ) {
+                var imgURL: NSURL = NSURL(string: urlString)
+                
+                // Download an NSData representation of the image at the URL
+                var imgData: NSData = NSData(contentsOfURL: imgURL)
+                image = UIImage(data: imgData)
+                self.imageCache.setValue(image, forKey: urlString)
+//                self.imageCache[urlString] = image
+            }
+            dispatch_async(dispatch_get_main_queue()) {
+                var sCell: UITableViewCell? = tableView.cellForRowAtIndexPath(indexPath)
+                if sCell != nil {
+                    let pCell: UITableViewCell = sCell!
+                    pCell.image = image
+                    println("set cell to \(image?.size.width)")
+                    cell.image = image
+                    
+                }
+                println("attempt to set cell")
+            }
+            
+        })
+
+        tableView.indexPathForCell(cell)
 
         cell.detailTextLabel.text = formattedPrice
         

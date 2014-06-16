7c39481fa316cf058a612cd03268b4f183d6397f
Jameson Quave <jquave@gmail.com>
2014-06-16 10:08:02 -0500

    Part 1

--- SwiftTutorial/ViewController.swift
+++ SwiftTutorial/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section:    Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
     }
 
 

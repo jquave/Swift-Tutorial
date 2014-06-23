197ade252b7547057ef9cf142923d0ac11e52883
Grzegorz Synowiec <mikoj@mynwt.eu>
2014-06-23 20:39:54 +0200

    checking if delegate is set

--- SwiftTutorial/APIController.swift
+++ SwiftTutorial/APIController.swift
@@ -20,40 +20,44 @@ class APIController: NSObject {
     }
     
     func searchItunesFor(searchTerm: String) {
-        
-        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
-        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-        
-        // Now escape anything else that isn't URL-friendly
-        let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
-        let urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=music&entity=album"
-        get(urlPath)
+        if self.delegate {
+            // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+            let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+            
+            // Now escape anything else that isn't URL-friendly
+            let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+            let urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=music&entity=album"
+            get(urlPath)
+        }
     }
     
     func lookupAlbum(collectionId: Int) {
-        get("https://itunes.apple.com/lookup?id=\(collectionId)&entity=song")
+        if self.delegate {
+            get("https://itunes.apple.com/lookup?id=\(collectionId)&entity=song")
+        }
     }
     
     func get(path: String) {
-        let url = NSURL(string: path)
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
-            println("Task completed")
-            if(error) {
-                // If there is an error in the web request, print it to the console
-                println(error.localizedDescription)
-            }
-            var err: NSError?
-            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
-            if(err?) {
-                // If there is an error parsing JSON, print it to the console
-                println("JSON Error \(err!.localizedDescription)")
-            }
-            var results = jsonResult["results"] as NSArray
-            // Now send the JSON result to our delegate object
-            self.delegate?.didReceiveAPIResults(jsonResult)
-            })
-        task.resume()
+        if self.delegate {
+            let url = NSURL(string: path)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+                println("Task completed")
+                if(error) {
+                    // If there is an error in the web request, print it to the console
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err?) {
+                    // If there is an error parsing JSON, print it to the console
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                var results = jsonResult["results"] as NSArray
+                // Now send the JSON result to our delegate object
+                self.delegate?.didReceiveAPIResults(jsonResult)
+                })
+            task.resume()
+        }
     }
-    
 }

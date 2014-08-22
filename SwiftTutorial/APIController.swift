83f727542eecd9988cc3d1fa19bd49ef687bb109
Jameson Quave <jquave@gmail.com>
2014-08-22 02:11:39 -0500

    Part3

--- SwiftTutorial/APIController.swift
+++ SwiftTutorial/APIController.swift
@@ -1,51 +1,23 @@
 //
-//  ViewController.swift
+//  APIController.swift
 //  SwiftTutorial
 //
 //  Created by Jameson Quave on 8/22/14.
 //  Copyright (c) 2014 JQ Software LLC. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController {
     
-    @IBOutlet var appsTableView : UITableView?
-    var tableData = []
-                            
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        searchItunesFor("JQ Software")
+    var delegate: APIControllerProtocol?
+    
+    init() {
     }
-    
-    /// MARK: UITableViewDataSource, UITableViewDelegate methods
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
-    }
-    
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
-        
-        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
-        
-        cell.textLabel.text = rowData["trackName"] as String
-        
-        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
-        let urlString: NSString = rowData["artworkUrl60"] as NSString
-        let imgURL: NSURL = NSURL(string: urlString)
-        
-        // Download an NSData representation of the image at the URL
-        let imgData: NSData = NSData(contentsOfURL: imgURL)
-        cell.imageView.image = UIImage(data: imgData)
-        
-        // Get the formatted price string for display in the subtitle
-        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
-        
-        cell.detailTextLabel.text = formattedPrice
-        
-        return cell
-    }
-    
     
     func searchItunesFor(searchTerm: String) {
         
@@ -71,16 +43,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                     println("JSON Error \(err!.localizedDescription)")
                 }
                 let results: NSArray = jsonResult["results"] as NSArray
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.tableData = results
-                    self.appsTableView!.reloadData()
-                })
+                self.delegate?.didReceiveAPIResults(jsonResult)
+                
             })
             
             task.resume()
         }
     }
-
-
+    
 }
-

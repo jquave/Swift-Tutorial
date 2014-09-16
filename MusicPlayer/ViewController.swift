41763f43392a9da2cbdcace7ac694bc8555e8e2f
Jameson Quave <jquave@gmail.com>
2014-09-16 16:26:04 -0500

    Part 2

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var appsTableView : UITableView?
+    var tableData = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchItunesFor("JQ Software")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,16 +24,66 @@ class ViewController: UIViewController {
     }
     
     
-    func tableView(tableView: UITableView!, numberOfRowsInSection section:    Int) -> Int {
-        return 10
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
     }
+    
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel?.text = rowData["trackName"] as? String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        let imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.imageView?.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
         
         return cell
+    }
+    
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url: NSURL = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+                println("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) {
+                    // If there is an error parsing JSON, print it to the console
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                let results: NSArray = jsonResult["results"] as NSArray
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableData = results
+                    self.appsTableView!.reloadData()
+                })
+            })
+            
+            task.resume()
+        }
     }
 
 

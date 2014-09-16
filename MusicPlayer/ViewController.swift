5d6e14005f7aced8f7a72b289c3b0a939800d4e3
Jameson Quave <jquave@gmail.com>
2014-09-16 16:21:08 -0500

    Part 1

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -19,6 +19,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section:    Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
 
 
 }

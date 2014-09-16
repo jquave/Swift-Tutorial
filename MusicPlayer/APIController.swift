caf4d07ce0efd7f0b0c087fef09e3a138a8a6fa7
Jameson Quave <jquave@gmail.com>
2014-09-16 16:58:00 -0500

    Part 3

---
+++ MusicPlayer/APIController.swift
@@ -0,0 +1,53 @@
+//
+//  APIController.swift
+//  MusicPlayer
+//
+//  Created by Jameson Quave on 9/16/14.
+//  Copyright (c) 2014 JQ Software LLC. All rights reserved.
+//
+
+import Foundation
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController {
+    
+    var delegate: APIControllerProtocol?
+
+    init() {
+    }
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url: NSURL = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+                println("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) {
+                    // If there is an error parsing JSON, print it to the console
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                let results: NSArray = jsonResult["results"] as NSArray
+                self.delegate?.didReceiveAPIResults(jsonResult)
+            })
+            
+            task.resume()
+        }
+    }
+    
+}
---
+++ MusicPlayer/SearchResultsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Jameson Quave on 9/16/14.
+//  Copyright (c) 2014 JQ Software LLC. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
+    
+    @IBOutlet var appsTableView : UITableView?
+    var tableData = []
+    var api = APIController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        api.searchItunesFor("Angry Birds")
+        api.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel?.text = rowData["trackName"] as? String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        let imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.imageView?.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
+        
+        return cell
+    }
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = resultsArr
+            self.appsTableView!.reloadData()
+        })
+    }
+
+}
+

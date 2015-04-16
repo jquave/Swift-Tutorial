0f28c1223c0776edd9a4e4afee279646b122336b
Jameson Quave <jquave@gmail.com>
2015-04-16 13:59:43 -0500

    Initial Commit

---
+++ iTunesPreviewTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesPreviewTutorial
+//
+//  Created by Jameson Quave on 4/16/15.
+//  Copyright (c) 2015 JQ Software LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

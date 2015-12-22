a1d6696f424877be4a579119b141cd7c869203ef
Jameson Quave <jquave@gmail.com>
2015-12-22 17:21:25 -0600

    Part1Playground

---
+++ SwiftBasics.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+
+
+var myString = "This is my string."
+let someConstant = 40
+let someOtherConstant: Int = 40
+var colorsArray = ["Blue", "Red", "Green", "Yellow"]
+var colorsDictionary = ["PrimaryColor":"Green", "SecondaryColor":"Red"]
+
+
+let firstColor = colorsArray[0]
+let aColor = colorsDictionary["PrimaryColor"]

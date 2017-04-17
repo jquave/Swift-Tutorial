7fd36078544b3126248da9871d64320dde0716b8
Jameson Quave <jquave@gmail.com>
2017-04-17 10:50:16 -0500

    Initial Commit

---
+++ DevelopingiOSAppsUsingSwiftTutorial-Part1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevelopingiOSAppsUsingSwiftTutorial-Part1
+//
+//  Created by Jameson Quave on 4/17/17.
+//  Copyright © 2017 Jameson Quave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
